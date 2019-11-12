910874a2e277880e5d24b4a1b9acd1e01580f0ae
YuG56 <963419094@qq.com>
2019-11-12 23:52:27 +0800

    No.27 新增字典序

--- 剑指offer/字符串排序.cpp
+++ 剑指offer/字符串排序.cpp
@@ -38,3 +38,64 @@ public:
 };
 
 //方法二：字典序全排序列
+class Solution {
+public:
+    作者：yanyan090123
+链接：https://www.nowcoder.com/questionTerminal/fe6b651b66ae47d7acce78ffdd9a96c7?f=discussion
+来源：牛客网
+
+vector<string> Permutation(string str) {
+        vector<string> res;
+        int len = str.length();
+        if(len==0){
+            return res;
+        }else if(len==1){
+            res.push_back(str);
+            return res;
+        }
+        vector<char> char_list;
+        int i,j;
+        for(i=0;i<len;i++){
+            char_list.push_back(str[i]);
+        }
+        sort(char_list.begin(),char_list.end());
+        for(i=0;i<len;i++){
+            str[i]  = char_list[i];
+        }
+        res.push_back(str);
+        int left,right;
+        char min_one;
+        while(true){
+            left=-1;
+            for(j=len-1;j>=1;j--){
+                if(str[j-1]<str[j]){
+                    left = j-1;
+                    break;
+                }
+            }
+            
+            if(left==-1){
+                break;
+            }
+            min_one = str[left+1];
+            right = left+1;
+            for(j = left+2;j<len;j++){
+                if(str[j]>str[left]&&str[j]<min_one){
+                    min_one = str[j];
+                    right = j;
+                }
+            }
+            swap(str[left],str[right]);
+            for(i =left+1,j=len-1;i<j;i++,j--){
+                swap(str[i],str[j]);
+                
+            }
+            res.push_back(str);
+        }
+        
+        return res;
+    }
+    
+    
+};
+};
