a5342c967e1c85b85a7cb6842bb7a552770e5c72
YuG <963419094@qq.com>
2020-02-18 19:07:47 +0800

    no.167 --二分查找？

--- CPP/167.两数之和II-输入有序数组.cpp
+++ CPP/167.两数之和II-输入有序数组.cpp
@@ -17,3 +17,22 @@ public:
         return res;
     }
 };
+
+// O(n)解法双指针，从两侧向中间滑动
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int size = int(numbers.size());
+        int low(0), high(size - 1);
+        while(low < high)
+        {
+            int sum = numbers[low] + numbers[high];
+            if(sum == target)
+                return {low + 1, high + 1};
+            else if(sum < target)
+                low++;
+            else high--;
+        }
+        return {-1, -1};
+    }
+};
