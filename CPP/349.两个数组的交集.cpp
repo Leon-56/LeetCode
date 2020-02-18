cdc7f6ef4f7a6f1c9ba7b738b96852b357fc3d3a
YuG <963419094@qq.com>
2020-02-18 22:41:36 +0800

    no.349 --哈希

---
+++ CPP/349.两个数组的交集.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int size1 = int(nums1.size()), size2 = int(nums2.size());
+        vector<int> res;
+        if(size1 == 0 || size2 == 0)
+            return res;
+        map<int, int> m_map;
+        for(int i = 0; i < size1; i++)
+        {
+            if(m_map.count(nums1[i]) == 0)
+                m_map[nums1[i]] = 1;
+        }
+        for(int i = 0; i < size2; i++)
+        {
+            if(m_map.count(nums2[i]) && m_map[nums2[i]] == 1)
+            {
+                m_map[nums2[i]]++;
+                res.push_back(nums2[i]);
+            }
+                
+        }
+        return res;
+    }
+};
