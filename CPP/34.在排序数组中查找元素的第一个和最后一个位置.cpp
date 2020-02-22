4a7557354c85397bf47a25b9535b0492b44fd238
YuG <963419094@qq.com>
2020-02-22 16:55:13 +0800

    no.34 解法2 --二分查找

--- CPP/34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ CPP/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -24,3 +24,36 @@ public:
         else return {-1, -1};
     }
 };
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2, -1);
+        int size = int(nums.size());
+        if(size == 0)
+            return res;
+        int left(0), right(size - 1);
+        // 寻找左边界
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < target)
+                left = mid + 1;
+            else right = mid;
+        }
+        if(nums[left] != target)
+            return res;
+        res[0] = left;
+        right = size;
+        // 寻找右边界
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] <= target)
+                left = mid + 1;
+            else right = mid;
+        }
+        res[1] = left - 1;
+        return res;
+    }
+};
